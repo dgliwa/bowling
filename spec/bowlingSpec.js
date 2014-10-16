ca1ce3f70adc84e922c62bfecbef8713f0d7eb47
derekgilwa <dgliwa@thoughtworks.com>
2014-10-16 17:36:23 -0500

    updated game to be able to complete perfect 300 game

--- spec/bowlingSpec.js
+++ spec/bowlingSpec.js
@@ -3,13 +3,13 @@ describe("Bowling Analyzer", function () {
 
     });
 
-    it("should set the score to 9 and move to the second frame after a roll of 7 and 2", function(){
+    it("should set the score() to 9 and move to the second frame after a roll of 7 and 2", function(){
       var game = new BowlingGame();
 
       game.roll(7);
       game.roll(2);
-      expect(game.score).toEqual(9);
-      expect(game.game_frame).toEqual(2);
+      expect(game.score()).toEqual(9);
+      expect(game.framesCompleted()).toEqual(1);
     });
 
     it("should add the first roll of the frame after a spare twice", function(){
@@ -20,7 +20,7 @@ describe("Bowling Analyzer", function () {
 
       game.roll(3);
       game.roll(4);
-      expect(game.score).toEqual(20);
+      expect(game.score()).toEqual(20);
     });
 
     it("should add the both rolls of the next frame after a strike twice", function(){
@@ -30,10 +30,10 @@ describe("Bowling Analyzer", function () {
 
       game.roll(3);
       game.roll(4);
-      expect(game.score).toEqual(24);
+      expect(game.score()).toEqual(24);
     });
 
-    it("should properly compute score after 2 strikes and a frame.", function(){
+    it("should properly compute score() after 2 strikes and a frame.", function(){
       var game = new BowlingGame();
 
       game.roll(10);
@@ -41,10 +41,10 @@ describe("Bowling Analyzer", function () {
       game.roll(10);
       game.roll(4);
       game.roll(4);
-      expect(game.score).toEqual(50);
+      expect(game.score()).toEqual(50);
     });
 
-    it("should properly compute score after 3 strikes and a frame.", function(){
+    it("should properly compute score() after 3 strikes and a frame.", function(){
       var game = new BowlingGame();
 
       game.roll(10);
@@ -53,7 +53,47 @@ describe("Bowling Analyzer", function () {
 
       game.roll(4);
       game.roll(4);
-      expect(game.score).toEqual(80);
+      expect(game.score()).toEqual(80);
     });
 
+    it("should stay in 10th frame after a strike on the first roll.", function(){
+      var game = new BowlingGame();
+
+      game.total_score = [1,1,1,1,1,1,1,1,1];
+
+      game.roll(10);
+      game.roll(2);
+      expect(game.framesCompleted()).toEqual(9);
+    });
+
+    it("should not add to score() once the game is over.", function(){
+      var game = new BowlingGame();
+
+      game.total_score = [0,0,0,0,0,0,0,0,0];
+
+      game.roll(7);
+      game.roll(2);
+
+      game.roll(1);
+      game.roll(1);
+      expect(game.score()).toEqual(9);
+    });
+
+    it("should bowl 300", function(){
+      var game = new BowlingGame();
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+      game.roll(10);
+
+      expect(game.score()).toEqual(300);
+    });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,6 +1,5 @@
 var BowlingGame = function(){
-  this.score = 0;
-  this.game_frame = 1;
+  this.total_score = [];
   this.frame_roll = 0;
   this.frame_score = 0;
   this.bonus_pins = 0;
@@ -10,28 +9,64 @@ var BowlingGame = function(){
 
 
 BowlingGame.prototype.roll=function(pins) {
+  if(!this.gameOver()){
     if(this.multiplier.length !== 0){
       this.bonus_pins += pins * this.multiplier.shift() - pins;
     }
     this.frame_score += pins;
-    this.frame_roll = (this.frame_roll + 1) % 2;
-    if(this.frame_roll === 0 || pins === 10){
-      this.game_frame += 1;
-      this.score += this.frame_score;
-      this.score += this.bonus_pins;
-      if(pins === 10){
-        if(this.multiplier[0] > 0){
-          this.multiplier[0] += 1;
-        }
-        else{
-        this.multiplier.push(2);
+
+    if(this.isTenthFrameAndMarks(pins)){
+      this.frame_roll = (this.frame_roll + 1) % 3;
+      if(this.frame_roll === 2){
+        this.multiplier = [];
       }
-      }
-      if(this.frame_score === 10){
-        this.multiplier.push(2);
-      }
+    }
+    else if(this.isMark(pins)){
+      this.frame_roll = 0;
+      this.computeMultiplier(pins);
+    }
+    else{
+      this.frame_roll = (this.frame_roll + 1) % 2;
+    }
+    if(this.frame_roll === 0){
+      this.total_score.push(this.frame_score + this.bonus_pins);
       this.frame_score = 0;
       this.bonus_pins = 0;
       this.frame_roll = 0;
     }
+  }
+}
+
+BowlingGame.prototype.gameOver=function(){
+  return this.total_score.length === 10;
+}
+
+BowlingGame.prototype.isTenthFrameAndMarks=function(pins){
+  return (pins === 10 || this.frame_score >= 10) && this.framesCompleted() === 9;
+}
+
+BowlingGame.prototype.isMark=function(pins){
+  return pins === 10 || this.frame_score === 10;
+}
+
+BowlingGame.prototype.framesCompleted=function(){
+  return this.total_score.length;
+}
+
+BowlingGame.prototype.score=function(){
+   return _.reduce(this.total_score, function(score, frame){ return score + frame; }, 0);
+}
+
+BowlingGame.prototype.computeMultiplier=function(pins){
+  if(pins === 10){
+    if(this.multiplier[0] > 0){
+      this.multiplier[0] += 1;
+    }
+    else{
+      this.multiplier.push(2);
+    }
+  }
+  if(this.frame_score === 10){
+    this.multiplier.push(2);
+  }
 }
